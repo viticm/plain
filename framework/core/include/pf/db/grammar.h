cf39ec5b4cf8909b2d07df8120722ec550314d95
viticm <viticm.ti@gmail.com>
2017-11-29 21:16:47 +0800

    The database garmmar class contract add

---
+++ framework/core/include/pf/db/grammar.h
@@ -0,0 +1,74 @@
+/**
+ * PLAIN FRAMEWORK ( https://github.com/viticm/plainframework )
+ * $Id grammar.h
+ * @link https://github.com/viticm/plainframework for the canonical source repository
+ * @copyright Copyright (c) 2014- viticm( viticm.ti@gmail.com )
+ * @license
+ * @user viticm<viticm.ti@gmail.com>
+ * @date 2017/11/29 19:56
+ * @uses The contract class of database garmmar.
+*/
+#ifndef PF_DB_GRAMMAR_H_
+#define PF_DB_GRAMMAR_H_
+
+#include "pf/db/config.h"
+#include "pf/basic/type/variable.h"
+
+namespace pf_db {
+
+class Grammar {
+
+ public:
+   Grammar() {};
+   virtual Grammar() {};
+
+ public:
+   
+   //Set the grammar's table prefix.
+   void set_table_prefix(const std::string &prefix);
+
+   //Get the grammar's table prefix.
+   const std::string get_table_prefix() const {
+      return table_prefix_; 
+   };
+
+   //Wrap an array of values.
+   void wrap_array(variable_array_t &values);
+
+   //Wrap a table in keyword identifiers.
+   void wrap_table(variable_t &table);
+
+   //Wrap a value in keyword identifiers.
+   void wrap(variable_t &value, bool prefix_alias = false);
+
+   //Wrap a value that has an alias.
+   void wrap(variable_t &value, bool prefix_alias = false);
+
+   //Wrap the given value segments.
+   void wrap_segments(variable_array_t &segments);
+
+   //Wrap a single string in keyword identifiers.
+   void wrap_value(variable_t &value);
+
+   //Convert an array of column names into a delimited string.
+   const std::string columnize(const variable_array_t &columns) const;
+
+   //Create query parameter place-holders for an array.
+   const std::string parameterize(const variable_array_t &values);
+
+   //Get the appropriate query parameter place-holder for a value.
+   const std::string parameter(const variable_t &value);
+
+ public:
+   using namespace pf_basic::type;
+
+ protected:
+
+   //The grammar table prefix.
+   std::string table_prefix_;
+
+};
+
+}; //namespace pf_db
+
+#endif //PF_DB_GRAMMAR_H_
---
+++ framework/core/include/pf/db/query/expression.h
@@ -0,0 +1,38 @@
+/**
+ * PLAIN FRAMEWORK ( https://github.com/viticm/plainframework )
+ * $Id expression.h
+ * @link https://github.com/viticm/plainframework for the canonical source repository
+ * @copyright Copyright (c) 2014- viticm( viticm.ti@gmail.com )
+ * @license
+ * @user viticm<viticm.ti@gmail.com>
+ * @date 2017/11/29 20:56
+ * @uses your description
+*/
+#ifndef PF_DB_QUERY_EXPRESSION_H_
+#define PF_DB_QUERY_EXPRESSION_H_
+
+#include "pf/db/query/config.h"
+
+namespace pf_db {
+
+class Expression {
+
+ public:
+   Expression(const std::string &value) : value_{value} {};
+   ~Expression() {};
+
+ public:
+
+   //Get the value of the expression.
+   const std::string get_value() const { return value_; };
+
+ protected:
+
+   //The value of the expression.
+   std::string value_;
+
+};
+
+}; //namespace pf_db
+
+#endif //PF_DB_QUERY_EXPRESSION_H_
