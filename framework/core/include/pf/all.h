28f9411de4507735da340a341211570992407a01
leafly <leafly.ti@hotmail.com>
2019-03-26 20:29:33 +0800

    Add the licensing note

--- framework/core/include/pf/all.h
+++ framework/core/include/pf/all.h
@@ -9,6 +9,10 @@
  * @uses The framework all inlcudes.
  *       This group defines just can define one.
  *             group1: PF_OPEN_ICOP|PF_OPEN_EPOLL
+ *
+ * ------------------------------------------------------------------------------
+ *  Licensing information can be found at the end of the file.
+ * ------------------------------------------------------------------------------
 */
 #ifndef PF_H_
 #define PF_H_
@@ -124,3 +128,27 @@
 #include "pf/util/random.h"
 
 #endif //PF_H_
+
+/*
+MIT License
+
+Copyright (c) 2017 viticm
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
