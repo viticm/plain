e38307916c4c84d6e9bf1b23b852f14c93d944e7
viticm <viticm@126.com>
2022-02-08 11:15:17 +0800

    Fix LF console net output on windows

--- framework/core/src/console/net_output.cc
+++ framework/core/src/console/net_output.cc
@@ -7,7 +7,7 @@ void NetOutput::write(
     const std::string &messages, bool newline, uint16_t options) {
   UNUSED(options);
   if (is_null(connection_) || connection_->is_disconnect()) return;
-  std::string str = newline ? messages + "\n" : messages;
+  std::string str = newline ? messages + LF : messages;
   // std::cout << "messages: " << str << std::endl;
   auto ostream = &connection_->ostream();
   ostream->write(str.c_str(), (uint32_t)str.size());
