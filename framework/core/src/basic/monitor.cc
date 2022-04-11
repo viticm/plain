0ac3b4f15f329b8a77ba81d9c4698b317814672b
viticm <viticm@126.com>
2022-04-11 19:30:17 +0800

    Opitimize code

--- framework/core/src/basic/monitor.cc
+++ framework/core/src/basic/monitor.cc
@@ -14,7 +14,7 @@ monitor::~monitor() {
   auto end_time = std::clock();
   auto diff_time = 1000.0 * (end_time - start_time_) / CLOCKS_PER_SEC;
   io_cdebug("[monitor] (%s) run time: %.3f", name_.c_str(), diff_time);
-  if (time_ > 0 && static_cast<int32_t>(diff_time) > time_) {
+  if (time_ > 0 && static_cast<uint32_t>(diff_time) > time_) {
     SLOW_WARNINGLOG(
         "monitor", "[monitor] %s cost %.3f", name_.c_str(), diff_time);
   }
