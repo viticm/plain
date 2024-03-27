de18eb27bb205d47871a0f8f4f5089e777036dbf
viticm <viticm@126.com>
2024-03-27 15:23:00 +0800

    Optimized

--- framework/core/src/sys/thread.cc
+++ framework/core/src/sys/thread.cc
@@ -66,28 +66,6 @@ ThreadPool::ThreadPool(size_t threads)
     );
 }
 
-// add new work item to the pool
-template<class F, class... Args>
-auto ThreadPool::enqueue(F&& f, Args&&... args) 
-  -> std::future<typename std::invoke_result_t<F, Args...>> {
-  using return_type = typename std::invoke_result_t<F, Args...>;
-  auto task = std::make_shared< std::packaged_task<return_type()> >(
-      std::bind(std::forward<F>(f), std::forward<Args>(args)...)
-    );
-  std::future<return_type> res = task->get_future();
-  {
-    std::unique_lock<std::mutex> lock(queue_mutex_);
-
-    // don't allow enqueueing after stopping the pool
-    if (stop_)
-      throw std::runtime_error("enqueue on stopped ThreadPool");
-
-    tasks_.emplace([task](){ (*task)(); });
-  }
-  condition_.notify_one();
-  return res;
-}
-
 // the destructor joins all threads
 ThreadPool::~ThreadPool() {
   {
