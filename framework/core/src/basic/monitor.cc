d72d23188573ceea15837f64546668f55f60f5a3
viticm <viticm@126.com>
2024-03-29 16:10:00 +0800

    Optimized

--- framework/core/src/basic/monitor.cc
+++ framework/core/src/basic/monitor.cc
@@ -7,13 +7,13 @@ using namespace plain;
 monitor::monitor(const std::string &name, uint32_t time) : 
   name_{name}, time_{time} {
   start_time_ = std::clock();
-  io_cdebug("[monitor] (%s) start ...", name_.c_str());
+  io_cdebug("[monitor] ({}) start ...", name_);
 }
 
 monitor::~monitor() {
   auto end_time = std::clock();
   auto diff_time = 1000.0 * (end_time - start_time_) / CLOCKS_PER_SEC;
-  io_cdebug("[monitor] (%s) run time: %.3f", name_.c_str(), diff_time);
+  io_cdebug("[monitor] ({}) run time: {}", name_, diff_time);
   if (time_ > 0 && static_cast<uint32_t>(diff_time) > time_) {
     //SLOW_WARNINGLOG(
     //    "monitor", "[monitor] %s cost %.3f", name_.c_str(), diff_time);
