d59417ba3726317573acc7534df61f269f84701f
viticm <viticm@126.com>
2024-05-12 13:10:10 +0800

    Optimized

--- framework/core/include/plain/file/library.h
+++ framework/core/include/plain/file/library.h
@@ -29,18 +29,18 @@ class PLAIN_API Library {
    ~Library() { unload(); };
 
  public:
-   std::string errorstr() const { return errorstr_; };
-   std::string filename() const { return filename_; }
-   void set_filename(const std::string &filename);
-   void set_path(const std::string &path) {
+   std::string errorstr() const noexcept { return errorstr_; };
+   std::string filename() const noexcept { return filename_; }
+   void set_filename(const std::string &filename) noexcept;
+   void set_path(const std::string &path) noexcept {
      path_ = path;
    };
 
  public:
-   bool isloaded() const { return isloaded_; };
-   bool load(bool tryprefix = true, bool seeglb = false);
-   bool unload();
-   void *resolve(const std::string &symbol, bool again = true);
+   bool isloaded() const noexcept { return isloaded_; };
+   bool load(bool tryprefix = true, bool seeglb = false) noexcept;
+   bool unload() noexcept;
+   void *resolve(const std::string &symbol, bool again = true) noexcept;
 
  private:
    std::string errorstr_;
@@ -54,68 +54,69 @@ class PLAIN_API Library {
 class PLAIN_API LibraryManager {
 
  public:
-   LibraryManager();
-   ~LibraryManager();
+  LibraryManager();
+  ~LibraryManager();
 
  public:
    
-   /* Set the search paths can find the librarys. */
-   void set_searchpaths(const std::vector<std::string> &paths) {
-     searchpaths_ = paths;
-   }
+  /* Set the search paths can find the librarys. */
+  void set_searchpaths(const std::vector<std::string> &paths) noexcept {
+    searchpaths_ = paths;
+  }
 
-   /* Add the search paths can find the librarys. */
-   void add_searchpaths(const std::vector<std::string> &paths);
+  /* Add the search paths can find the librarys. */
+  void add_searchpaths(const std::vector<std::string> &paths) noexcept;
 
-   /* Remove the search paths can find the librarys. */
-   void remove_searchpaths(const std::vector<std::string> &paths);
+  /* Remove the search paths can find the librarys. */
+  void remove_searchpaths(const std::vector<std::string> &paths) noexcept;
 
-   /* Get all current search paths. */
-   std::vector<std::string> get_searchpaths() {
-     return searchpaths_;
-   }
+  /* Get all current search paths. */
+  std::vector<std::string> get_searchpaths() const noexcept {
+    return searchpaths_;
+  }
 
-   /* Set the load library all possible names. 
-    * e.g onlyname "OPGL"
-    *     names "OpenGL", "OpenGL32"
-    * */
-   void set_librarynames(const std::string &onlyname, 
-                         const std::vector<std::string> &names) {
-     namesmap_[onlyname] = names;
-   }
+  /* Set the load library all possible names. 
+  * e.g onlyname "OPGL"
+  *     names "OpenGL", "OpenGL32"
+  * */
+  void set_librarynames(
+    const std::string &onlyname, const std::vector<std::string> &names) {
+    namesmap_[onlyname] = names;
+  }
 
-   /* Remove the load library all possible names(look up set). */
-   void remove_librarynames(const std::string &onlyname, 
-                            const std::vector<std::string> &names);
+  /* Remove the load library all possible names(look up set). */
+  void remove_librarynames(
+    const std::string &onlyname, const std::vector<std::string> &names);
 
-   /* Get the load library all possible names. */
-   std::vector<std::string> get_librarynames(const std::string &onlyname) {
-     return namesmap_[onlyname];
-   }
+  /* Get the load library all possible names. */
+  std::vector<std::string>
+  get_librarynames(const std::string &onlyname) noexcept {
+   return namesmap_[onlyname];
+  }
 
-   /* Load the library by name. */
-   bool load(const std::string &name, 
-             bool seeglb = false,
-             const variable_array_t &params = {});
+  /* Load the library by name. */
+  bool load(
+    const std::string &name, bool seeglb = false,
+    const variable_array_t &params = {}) noexcept;
 
-   /* Unload the library by name. */
-   bool unload(const std::string &name);
+  /* Unload the library by name. */
+  bool unload(const std::string &name) noexcept;
 
-   /* Get the library pointer by name. */
-   Library *get(const std::string &name) {
-     return is_null(librarymap_[name]) ? nullptr : librarymap_[name].get();
-   }
+  /* Get the library pointer by name. */
+  Library *get(const std::string &name) noexcept {
+   return is_null(librarymap_[name]) ? nullptr : librarymap_[name].get();
+  }
 
- private:
-   
-   /* The search paths. */
-   std::vector<std::string> searchpaths_;
+  private:
 
-   /* The names map. */
-   std::unordered_map<std::string, std::vector<std::string>> namesmap_;
+  /* The search paths. */
+  std::vector<std::string> searchpaths_;
 
-   /* The library object pointer map. */
-   std::unordered_map<std::string, std::shared_ptr<Library>> librarymap_;
+  /* The names map. */
+  std::unordered_map<std::string, std::vector<std::string>> namesmap_;
+
+  /* The library object pointer map. */
+  std::unordered_map<std::string, std::shared_ptr<Library>> librarymap_;
 
 };
 
